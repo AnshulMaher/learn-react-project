f73133827fd4d907ff351933798075fe6698dc2f
Anshul Maher <anshulmaher016@gmail.com>
2022-02-20 10:02:17 +0530

    added error boundary and code splitting

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Homepage from './pages/homepage/homepage';
-import CardDetailPage from './pages/card-detail/card-detail';
-import './App.css';
 import Header from './components/header/header';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 import RequireAuth from './components/require-auth.component';
 import Notification from './components/notification/notification';
+import Loader from './components/loader/loader';
+import ErrorBoundary from './components/error-boundary/error-boundary.component';
+
+import './App.css';
+
+const Homepage = lazy(() => import('./pages/homepage/homepage'));
+const CardDetailPage = lazy(() => import('./pages/card-detail/card-detail'));
+const SignInAndSignUpPage = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up'));
 
 function App() {
   return (
     <div className="App">
-      <Notification />
-      <Header />
-      <Routes>
-        <Route element={<RequireAuth />}>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/detail/:id" element={<CardDetailPage />} />
-        </Route>
+        <Notification />
+        <Header />
+      <ErrorBoundary>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route element={<RequireAuth />}>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/detail/:id" element={<CardDetailPage />} />
+            </Route>
 
-        <Route path="/sign-in" element={<SignInAndSignUpPage />} />
-      </Routes>
+            <Route path="/sign-in" element={<SignInAndSignUpPage />} />
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
